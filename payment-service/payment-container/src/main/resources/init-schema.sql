92d0e3ad8023a5ff23ff4f7ecef018ee9acc1417
Eray Aydın <erayaydinn@protonmail.com>
2022-12-23 18:35:57 +0300

    feat: test for payment request message listener

--- payment-service/payment-container/src/main/resources/init-schema.sql
+++ payment-service/payment-container/src/main/resources/init-schema.sql
@@ -53,7 +53,7 @@ CREATE TABLE "payment".order_outbox
     id uuid NOT NULL,
     saga_id uuid NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    processed_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
     type character varying COLLATE pg_catalog."default" NOT NULL,
     payload jsonb NOT NULL,
     outbox_status outbox_status NOT NULL,
